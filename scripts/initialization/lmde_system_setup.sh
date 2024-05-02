3b5407a93963bf0d1e6daa9d1445326d1827781d
DSikes <pockybum522@gmail.com>
2024-05-02 17:27:29 -0400

    Script works completely without crashing now

--- scripts/initialization/lmde_system_setup.sh
+++ scripts/initialization/lmde_system_setup.sh
@@ -3,6 +3,14 @@
 set -e
 set -o pipefail
 
+function run-in-user-session() {
+    _display_id=":$(find /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+    _username=$(who | grep "\(${_display_id}\)" | awk '{print $1}')
+    _user_id=$(id -u "$_username")
+    _environment=("DISPLAY=$_display_id" "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$_user_id/bus")
+    sudo -Hu "$_username" env "${_environment[@]}" "$@"
+}
+
 echo "### Changing grub timeout to 1s ###" ##############################################################################
 sed -i '/GRUB_TIMEOUT=5/c\GRUB_TIMEOUT=1' /etc/default/grub
 sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet"' /etc/default/grub.d/50_lmde.cfg
@@ -15,29 +23,108 @@ USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 mkdir -p "$USER_HOME/.local"
 APP_INSTALLS_DIR="$USER_HOME/.local/share"
 
-if [ ! -f "$APP_INSTALLS_DIR" ]; then
-    mkdir -p "$APP_INSTALLS_DIR"
-fi
+mkdir -p "$APP_INSTALLS_DIR"
 
 BEEPER_INSTALLS_DIR="$USER_HOME/.local/share/beeper"
-if [ ! -f "$BEEPER_INSTALLS_DIR" ]; then
-    mkdir -p "$BEEPER_INSTALLS_DIR"
-fi
+mkdir -p "$BEEPER_INSTALLS_DIR"
 
-KMONAD_INSTALL_DIR="$USER_HOME/.local/share/beeper"
-if [ ! -f "$KMONAD_INSTALL_DIR" ]; then
-    mkdir -p "$KMONAD_INSTALL_DIR"
-fi
+KMONAD_INSTALL_DIR="$USER_HOME/.local/share/kmonad"
+mkdir -p "$KMONAD_INSTALL_DIR"
+
+
+echo "### Adding better mirrors ###" ###############################################################################
+cat > "/etc/apt/sources.list.d/official-package-repositories.list"<<EOF 
+deb https://mirror.cs.jmu.edu/pub/linuxmint/packages faye main upstream import backport 
+
+deb http://atl.mirrors.clouvider.net/debian bookworm main contrib non-free non-free-firmware
+deb http://atl.mirrors.clouvider.net/debian bookworm-updates main contrib non-free non-free-firmware
+deb http://security.debian.org/ bookworm-security main contrib non-free non-free-firmware
+
+deb http://atl.mirrors.clouvider.net/debian bookworm-backports main contrib non-free non-free-firmware
+
+EOF
+
+
+echo "### Setting theme to dark mode ###" ##############################################################################
+run-in-user-session gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
+run-in-user-session gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y-Dark"
+run-in-user-session gsettings set org.cinnamon.theme name "Mint-Y-Dark"
+
+
+echo "### Setting xed settings ###" ##############################################################################
+run-in-user-session gsettings set org.x.editor.preferences.editor display-line-numbers true
+run-in-user-session gsettings set org.x.editor.preferences.editor scheme cobalt
+run-in-user-session gsettings set org.x.editor.preferences.editor bracket-matching true
+run-in-user-session gsettings set org.x.editor.preferences.editor highlight-current-line true
+run-in-user-session gsettings set org.x.editor.preferences.editor auto-indent true
+run-in-user-session gsettings set org.x.editor.plugins active-plugins "['sort', 'filebrowser', 'docinfo', 'joinlines', 'spell', 'textsize', 'open-uri-context-menu', 'modelines', 'time']"
+
+
+echo "### Setting update manager to hide icon unless updates are available ###" ##############################################################################
+run-in-user-session gsettings set com.linuxmint.updates hide-systray true
+
+echo "### Setting notification settings ###" ##############################################################################
+run-in-user-session gsettings set org.cinnamon.desktop.notifications bottom-notifications true
+run-in-user-session gsettings set org.cinnamon.desktop.notifications remove-old true
+run-in-user-session gsettings set org.cinnamon.desktop.notifications notification-duration 3
+
+
+echo "### Setting nemo file manager settings ###" ##############################################################################
+run-in-user-session gsettings set org.nemo.list-view default-zoom-level 'smaller'
+run-in-user-session gsettings set org.nemo.preferences default-folder-viewer 'list-view'
+run-in-user-session gsettings set org.nemo.preferences click-policy 'single'
+run-in-user-session gsettings set org.nemo.preferences start-with-dual-pane true
+run-in-user-session gsettings set org.nemo.preferences ignore-view-metadata true
+run-in-user-session gsettings set org.nemo.preferences click-double-parent-folder true
+run-in-user-session gsettings set org.nemo.preferences show-full-path-titles true
+run-in-user-session gsettings set org.nemo.preferences tooltips-on-desktop true
+run-in-user-session gsettings set org.nemo.preferences tooltips-show-file-type true
+run-in-user-session gsettings set org.nemo.preferences tooltips-show-mod-date true
+run-in-user-session gsettings set org.nemo.preferences tooltips-show-birth-date true
+run-in-user-session gsettings set org.nemo.preferences tooltips-show-access-date true
+run-in-user-session gsettings set org.nemo.preferences tooltips-show-path true
+run-in-user-session gsettings set org.nemo.preferences tooltips-in-list-view true
+run-in-user-session gsettings set org.nemo.preferences tooltips-in-icon-view true
+run-in-user-session gsettings set org.nemo.preferences show-new-folder-icon-toolbar true
+run-in-user-session gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
+run-in-user-session gsettings set org.nemo.preferences show-compact-view-icon-toolbar false
+run-in-user-session gsettings set org.nemo.preferences show-show-thumbnails-toolbar true
+run-in-user-session gsettings set org.nemo.preferences show-icon-view-icon-toolbar true
+run-in-user-session gsettings set org.nemo.preferences show-reload-icon-toolbar true
+run-in-user-session gsettings set org.nemo.preferences show-computer-icon-toolbar true
+run-in-user-session gsettings set org.nemo.preferences.menu-config selection-menu-make-link true
+run-in-user-session gsettings set org.nemo.preferences.menu-config selection-menu-duplicate true
+run-in-user-session gsettings set org.nemo.preferences show-location-entry true
+run-in-user-session gsettings set org.nemo.list-view default-column-order "['name', 'size', 'type', 'date_modified', 'date_created_with_time', 'date_accessed', 'date_created', 'detailed_type', 'group', 'where', 'mime_type', 'date_modified_with_time', 'octal_permissions', 'owner', 'permissions']"
+run-in-user-session gsettings set org.nemo.list-view default-visible-columns "['name', 'size', 'type', 'date_modified', 'date_created', 'owner']"
+run-in-user-session gsettings set org.nemo.window-state side-pane-view 'tree'
+
+
+echo "### Setting firefox keyboard shortcut ###" ##############################################################################
+run-in-user-session gsettings set org.cinnamon.desktop.keybindings custom-list "['__dummy__']"
+run-in-user-session gsettings set org.cinnamon.desktop.keybindings custom-list "['__dummy__', 'custom0']"
+run-in-user-session dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/binding "['<Alt>f']"
+run-in-user-session dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/command "'firefox'"
+run-in-user-session dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/name "'Firefox'"
+
+# Make sure dir exists
+mkdir -p "/media/secondary"
+chown david /media/secondary
+
+run-in-user-session gsettings set org.x.apps.favorites list "['computer:///::inode/directory', 'file:///home/david::inode/directory', 'file:///media/secondary::inode/directory', 'file:///media/secondary/Dropbox::inode/directory', 'file:///media/secondary/repos::inode/directory']"
 
 
 echo "### Downloading kmonad ###" ##############################################################################
-if [ ! -f "$KMONAD_INSTALL_DIR" ]; then
+if [ ! -d "$KMONAD_INSTALL_DIR" ]
+then
     curl https://raw.githubusercontent.com/PockyBum522/linux-files/master/bin/kmonad > "$KMONAD_INSTALL_DIR/kmonad"
+    chmod +x "$KMONAD_INSTALL_DIR/kmonad"    
     curl https://raw.githubusercontent.com/PockyBum522/linux-files/master/configuration/kmonad/default_layers.kbd > "$KMONAD_INSTALL_DIR/default_layers.kbd"
     groupadd uinput
     usermod -aG input,uinput david
     echo KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput" > /etc/udev/rules.d/80-kmonad.rules
     modprobe uinput
+    ./"$KMONAD_INSTALL_DIR/kmonad" &
 fi
 
 
@@ -50,90 +137,8 @@ apt-get -y upgrade
 apt-get -y dist-upgrade
 
 
-echo "### Setting theme to dark mode ###" ##############################################################################
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y-Dark"
-sudo -H -u david bash -c gsettings set org.cinnamon.theme name "Mint-Y-Dark"
-
-
-echo "### Setting xed settings ###" ##############################################################################
-sudo -H -u david bash -c gsettings set org.x.editor.preferences.editor display-line-numbers true
-sudo -H -u david bash -c gsettings set org.x.editor.preferences.editor scheme cobalt
-
-sudo -H -u david bash -c gsettings set org.x.editor.preferences.editor.bracket-matching true
-sudo -H -u david bash -c gsettings set org.x.editor.preferences.editor.highlight-current-line true
-sudo -H -u david bash -c gsettings set org.x.editor.preferences.editor.auto-indent true
-sudo -H -u david bash -c gsettings set org.x.editor.plugins.active-plugins "['sort', 'filebrowser', 'docinfo', 'joinlines', 'spell', 'textsize', 'open-uri-context-menu', 'modelines', 'time']"
-
-
-echo "### Setting update manager to hide icon unless updates are available ###" ##############################################################################
-sudo -H -u david bash -c gsettings set com.linuxmint.updates hide-systray true
-
-echo "### Setting notification settings ###" ##############################################################################
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.notifications bottom-notifications true
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.notifications remove-old true
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.notifications notification-duration 3
-
-
-echo "### Setting nemo file manager settings ###" ##############################################################################
-sudo -H -u david bash -c gsettings set org.nemo.list-view default-zoom-level 'smaller'
-sudo -H -u david bash -c gsettings set org.nemo.preferences default-folder-viewer 'list-view'
-sudo -H -u david bash -c gsettings set org.nemo.preferences click-policy 'single'
-sudo -H -u david bash -c gsettings set org.nemo.preferences start-with-dual-pane true
-sudo -H -u david bash -c gsettings set org.nemo.preferences ignore-view-metadata true
-sudo -H -u david bash -c gsettings set org.nemo.preferences click-double-parent-folder true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-full-path-titles true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-on-desktop true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-show-file-type true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-show-mod-date true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-show-birth-date true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-show-access-date true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-show-path true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-in-list-view true
-sudo -H -u david bash -c gsettings set org.nemo.preferences tooltips-in-icon-view true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-new-folder-icon-toolbar true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-compact-view-icon-toolbar false
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-show-thumbnails-toolbar true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-icon-view-icon-toolbar true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-reload-icon-toolbar true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-computer-icon-toolbar true
-sudo -H -u david bash -c gsettings set org.nemo.preferences menu-config.selection-menu-make-link true
-sudo -H -u david bash -c gsettings set org.nemo.preferences menu-config.selection-menu-duplicate true
-sudo -H -u david bash -c gsettings set org.nemo.preferences show-location-entry true
-sudo -H -u david bash -c gsettings set org.nemo.list-view default-column-order "['name', 'size', 'type', 'date_modified', 'date_created_with_time', 'date_accessed', 'date_created', 'detailed_type', 'group', 'where', 'mime_type', 'date_modified_with_time', 'octal_permissions', 'owner', 'permissions']"
-sudo -H -u david bash -c gsettings set org.nemo.list-view default-visible-columns "['name', 'size', 'type', 'date_modified', 'date_created', 'owner']"
-sudo -H -u david bash -c gsettings set org.nemo.window-state side-pane-view 'tree'
-
-
-echo "### Setting firefox keyboard shortcut ###" ##############################################################################
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.keybindings custom-list "['__dummy__']"
-sudo -H -u david bash -c gsettings set org.cinnamon.desktop.keybindings custom-list "['__dummy__', 'custom0']"
-sudo -H -u david bash -c dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/binding "['<Alt>f']"
-sudo -H -u david bash -c dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/command "'firefox'"
-sudo -H -u david bash -c dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/name "'Firefox'"
-
-# Make sure dir exists
-
-if [ ! -f "/media/secondary" ]; then
-    mkdir /media/secondary
-fi
-chown /media/secondary david
-
-sudo -H -u david bash -c gsettings set org.x.apps.favorites list "['computer:///::inode/directory', 'file:///home/david::inode/directory', 'file:///media/secondary::inode/directory', 'file:///media/secondary/Dropbox::inode/directory', 'file:///media/secondary/repos::inode/directory']"
-
-
 #echo "### Setting panel settings ###" ##############################################################################
-#sudo -H -u david bash -c 
-
-
-echo "### Setting system audio volume to 0 (Muted) ###" ################################################################
-xdotool key XF86AudioLowerVolume
-xdotool key XF86AudioLowerVolume
-xdotool key XF86AudioLowerVolume
-xdotool key XF86AudioLowerVolume
-xdotool key XF86AudioLowerVolume
-xdotool key XF86AudioLowerVolume
+#run-in-user-session 
 
 
 echo "### Installing common programs ###" ###############################################################################
@@ -148,11 +153,24 @@ apt install -y github-desktop
 apt install -y barrier
 apt install -y dropbox
 apt install -y flameshot
+apt install -y gparted
 apt install -y k4dirstat
 
 
 echo "### Installing common programs, flatpak edition ###" ###############################################################################
 flatpak install -y com.github.dail8859.NotepadNext
+
+
+echo "### Setting system audio volume to 0 (Muted) ###" ################################################################
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
+xdotool key XF86AudioLowerVolume
 
 
 # Language packs
@@ -161,7 +179,8 @@ apt install -y firefox-l10n-en
 
 echo "### Adding barrier config ###" ###############################################################################
 BARRIER_CONFIG_DIR="$USER_HOME/.config/Debauchee"
-if [ ! -f "$BARRIER_CONFIG_DIR" ]; then
+if [ ! -d "$BARRIER_CONFIG_DIR" ]
+then
     mkdir -p "$BARRIER_CONFIG_DIR"
     curl https://raw.githubusercontent.com/PockyBum522/linux-files/master/configuration/dotfiles/Barrier.conf > "$BARRIER_CONFIG_DIR/Barrier.conf"
 
@@ -197,7 +216,8 @@ curl https://raw.githubusercontent.com/PockyBum522/linux-files/master/configurat
 
 
 echo "### Disabling LMDE welcome dialog at startup ###" ################################################################
-if [ ! -f "$USER_HOME/.config/autostart/mintwelcome.desktop" ]; then
+if [ ! -d "$USER_HOME/.config/autostart/mintwelcome.desktop" ]
+then
     cat >> "$USER_HOME/.config/autostart/mintwelcome.desktop"<<EOF 
 [Desktop Entry]
 Encoding=UTF-8
@@ -216,13 +236,15 @@ fi
 
 
 echo "### Downloading latest beeper appimage ###" #########################################################################
-if [ ! -f "$BEEPER_INSTALLS_DIR/Beeper-Cloud.appimage" ]; then
+if [ ! -d "$BEEPER_INSTALLS_DIR/Beeper-Cloud.appimage" ]
+then
     curl https://download.beeper.com/linux/appImage/x64 > "$BEEPER_INSTALLS_DIR/Beeper-Cloud.appimage"
 fi
 
 chmod +x "$BEEPER_INSTALLS_DIR/Beeper-Cloud.appimage"
 
-if [ ! -f "$USER_HOME/Desktop/Beeper" ]; then
+if [ ! -f "$USER_HOME/Desktop/Beeper" ]
+then
     ln -s "$BEEPER_INSTALLS_DIR/Beeper-Cloud.appimage" "$USER_HOME/Desktop/Beeper"
 fi
 
@@ -260,7 +282,7 @@ else
 	echo -e "\n\e[32mDone! Running in a CI -- skipped launching the AppImage.\e[39m\n"
 fi
 
-sudo chown david $USER_HOME/.local/share"
+sudo chown david "$USER_HOME/.local/share"
 
 
 echo "### Adding $USER_HOME/.local/bin to PATH ###" ######################################################################
@@ -274,7 +296,7 @@ systemctl enable --now pcscd
 mkdir -p "$APP_INSTALLS_DIR/yubico-authenticator"
 
 if [ ! -f "$APP_INSTALLS_DIR/yubico-authenticator/Yubico-Authenticator.appimage" ]; then
-    curl https://developers.yubico.com/yubioath-flutter/Releases/yubioath-desktop-latest-linux.AppImage > "$APP_INSTALLS_DIR/Yubico-Authenticator/Yubico-Authenticator.appimage"
+    curl https://developers.yubico.com/yubioath-flutter/Releases/yubioath-desktop-latest-linux.AppImage > "$APP_INSTALLS_DIR/yubico-authenticator/Yubico-Authenticator.appimage"
 fi
 
 chmod +x "$APP_INSTALLS_DIR/yubico-authenticator/Yubico-Authenticator.appimage"
@@ -285,14 +307,14 @@ fi
 
 
 if [ "$HOSTNAME" = DAVID-LAPTOP ]; then
-    echo "### Installing nvidia driver ###" 
-    apt install -y nvidia-driver ;; ###########################################################################
+    echo "### Installing nvidia driver ###"  ###########################################################################
+    apt install -y nvidia-driver 
 
     # Set DPI fractional scaling on
-    gsettings set org.cinnamon.muffin experimental-features "['scale-monitor-framebuffer', 'x11-randr-fractional-scaling']"
+    run-in-user-session gsettings set org.cinnamon.muffin experimental-features "['scale-monitor-framebuffer', 'x11-randr-fractional-scaling']"
 fi
 
 if [ "$HOSTNAME" = DAVID-DESKTOP ]; then
-    echo "### Installing nvidia driver ###" 
-    apt install -y nvidia-driver ;; ###########################################################################
+    echo "### Installing nvidia driver ###" ###########################################################################
+    apt install -y nvidia-driver 
 fi
