ffda78dec51cb6a17b70f6cafa0d0c95210dc842
David S <1970959+PockyBum522@users.noreply.github.com>
2025-02-12 06:43:14 -0500

    Moved nemo right click handlers to own script for easy updating of just that on a system

--- scripts/initialization/lmde_system_setup.sh
+++ scripts/initialization/lmde_system_setup.sh
@@ -390,35 +390,4 @@ if [ "$HOSTNAME" = DAVID-DESKTOP ]; then
     run-in-user-session gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-battery 60
 fi
 
-
-echo "### Adding open withs to right click menu in nemo ###" #########################################################################
-
-nemo_actions_folder=/home/$USER/.local/share/nemo/actions
-
-# Add "open in vscode" to when you right click on an empty folder in linux
-printf "[Nemo Action]\nActive=true\nName=Open in VS Code\nComment=Opens current folder in VS Code\n" > "$nemo_actions_folder/vscode-open-folder.nemo_action"
-printf "Exec=code " >> "$nemo_actions_folder/vscode-open-folder.nemo_action"
-echo %P >> "$nemo_actions_folder/vscode-open-folder.nemo_action"
-printf "Icon-Name=code\nSelection=none\nExtensions=any;\nQuote=double\nDependencies=code;\n" >> "$nemo_actions_folder/vscode-open-folder.nemo_action"
-
-# Add open in PyCharm
-printf "[Nemo Action]\nActive=true\nName=Open Folder as PyCharm Project\nComment=Open Folder as PyCharm Project\n" > "$nemo_actions_folder/pycharm-open-folder.nemo_action"
-printf "Exec=/home/$USER/.local/share/JetBrains/Toolbox/apps/pycharm-professional/bin/pycharm " >> "$nemo_actions_folder/pycharm-open-folder.nemo_action"
-echo \"%P\" >> "$nemo_actions_folder/pycharm-open-folder.nemo_action"
-printf "Icon-Name=pycharm\nSelection=None\nExtensions=any\nTerminal=false\n" >> "$nemo_actions_folder/pycharm-open-folder.nemo_action"
-
-# Add open in clion
-printf "[Nemo Action]\nActive=true\nName=Open Folder as CLion Project\nComment=Open Folder as clion Project\n" > "$nemo_actions_folder/clion-open-folder.nemo_action"
-printf "Exec=/home/$USER/.local/share/JetBrains/Toolbox/apps/clion/bin/clion " >> "$nemo_actions_folder/clion-open-folder.nemo_action"
-echo \"%P\" >> "$nemo_actions_folder/clion-open-folder.nemo_action"
-printf "Icon-Name=clion\nSelection=None\nExtensions=any\nTerminal=false\n" >> "$nemo_actions_folder/clion-open-folder.nemo_action"
-
-# get the directory this script is being run from
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cp $SCRIPT_DIR/resources/icons/clion.svg /home/$USER/.local/share/icons/
-cp $SCRIPT_DIR/resources/icons/pycharm.svg /home/$USER/.local/share/icons/
-cp $SCRIPT_DIR/resources/icons/code.svg /home/$USER/.local/share/icons/
-
-
 echo "### Finished installing/configuring everything! ###"  ###########################################################################
