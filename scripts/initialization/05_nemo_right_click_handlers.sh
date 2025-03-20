890cb34792a27ec84474b1c40217f25e87e84970
DSikes <pockybum522@gmail.com>
2025-03-20 18:24:30 -0400

    Add open with rider icon copy and action copy

--- scripts/initialization/05_nemo_right_click_handlers.sh
+++ scripts/initialization/05_nemo_right_click_handlers.sh
@@ -9,10 +9,12 @@ RES_GITHUB=https://raw.githubusercontent.com/PockyBum522/linux-files/refs/heads/
 curl $RES_GITHUB/icons/clion.svg > /home/$USER/.local/share/icons/clion.svg
 curl $RES_GITHUB/icons/pycharm.svg > /home/$USER/.local/share/icons/pycharm.svg
 curl $RES_GITHUB/icons/code.svg > /home/$USER/.local/share/icons/code.svg
+curl $RES_GITHUB/icons/rider.svg > /home/$USER/.local/share/icons/rider.svg
 
 # Download the action files to the proper directory for nemo to start using them automatically
 curl $RES_GITHUB/nemo_actions/clion-open-folder.nemo_action > /home/$USER/.local/share/nemo/actions/clion-open-folder.nemo_action
 curl $RES_GITHUB/nemo_actions/create_link_on_desktop.nemo_action > /home/$USER/.local/share/nemo/actions/create_link_on_desktop.nemo_action
 curl $RES_GITHUB/nemo_actions/pycharm-open-folder.nemo_action > /home/$USER/.local/share/nemo/actions/pycharm-open-folder.nemo_action
 curl $RES_GITHUB/nemo_actions/vscode-open-folder.nemo_action > /home/$USER/.local/share/nemo/actions/vscode-open-folder.nemo_action
+curl $RES_GITHUB/nemo_actions/rider-open-folder.nemo_action > /home/$USER/.local/share/nemo/actions/rider-open-folder.nemo_action
 
