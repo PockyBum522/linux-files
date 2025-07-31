8ce514264d075db17821ec0c69edb20c3bd1f8e0
DSikes <pockybum522@gmail.com>
2025-07-31 05:50:20 -0400

    Adding several updates for DAVID-LAPTOP

--- scripts/keyboard/start_kmonad.sh
+++ scripts/keyboard/start_kmonad.sh
@@ -2,4 +2,5 @@ sudo modprobe uinput
 
 echo Running kmonad, ctrl + c to exit...
 
-sudo kmonad /media/secondary/repos/linux-files/configuration/kmonad/default_layers.kbd
+sudo kmonad /media/secondary/repos/linux-files/configuration/kmonad/david-laptop-numpad-remap.kbd
+
--- scripts/user_interface/wallpaper-slideshow-shuffle.sh
+++ scripts/user_interface/wallpaper-slideshow-shuffle.sh
@@ -5,7 +5,7 @@ export DISPLAY=:0
 # Script to randomly set Background from files in a directory
 
 # Directory Containing Pictures
-DIR="/media/secondary/seafile-libraries/Wallpapers/Single Screen/4k/Dark Only"
+DIR="/media/secondary/Wallpapers/Single Screen/4k/Dark Only"
 
 # Command to Select a random jpg file from directory
 # Delete the *.jpg to select any file but it may return a folder
