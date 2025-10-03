576ca6802db3d7257af4ca7e3a50f3adc17ce5ef
DSikes <pockybum522@gmail.com>
2025-10-03 09:22:50 -0400

    Made 4k and 1080 scripts for different computers

---
+++ scripts/user_interface/wallpaper-slideshow-shuffle-1080.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export DISPLAY=:0
+
+# Script to randomly set Background from files in a directory
+
+# Directory Containing Pictures
+DIR="/media/secondary/Wallpapers/Single Screen/1080p/Dark Only"
+
+# Command to Select a random jpg file from directory
+# Delete the *.jpg to select any file but it may return a folder
+PIC=$(ls "$DIR" | shuf -n 1)
+
+echo "$PIC"
+
+# Command to set Background Image
+/usr/bin/feh --bg-fill "$DIR/$PIC"
+
